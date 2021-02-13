33be9640a2f565a6ab368c46735009d64583fe80
Victor Chan <victorcnk@gmail.com>
2021-02-14 00:31:00 +0800

    packages

--- src/validate-bearer-token.js
+++ src/validate-bearer-token.js
@@ -5,7 +5,7 @@ function validateBearerToken(req, res, next) {
   const authToken = req.get('Authorization')
 
   console.log(authToken)
-  console.log("authToken splitted is " + authToken.split(' ')[1])
+  console.log("authToken spplitted is " + authToken.split(' ')[1])
   console.log("Original is " + API_TOKEN)
   if (!authToken || authToken.split(' ')[1] !== API_TOKEN) {
     logger.error(`Unauthorized request to path: ${req.path}`)
